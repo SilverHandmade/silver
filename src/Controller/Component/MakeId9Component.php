d189d67c5d98507ff5fc1635a179b05710f1b700
gratia1107 <oic.m.hida@gmail.com>
2017-11-15 13:13:57 +0900

    ID生成111513

--- src/Controller/Component/MakeId9Component.php
+++ src/Controller/Component/MakeId9Component.php
@@ -3,29 +3,52 @@
 namespace App\Controller\Component;
 
 use Cake\Controller\Component;
+use Cake\ORM\TableRegistry;
 
 class MakeId9Component extends Component {
 
+	//initializeに入れる↓
+	//$this->loadComponent('MakeId9');
+
+	//$a=$this->MakeId9->id9('req');　　id9('テーブル名頭3文字')
 	public function id9($tbl) {
-		if($tbl == 'req')
-			$TId ='requests';
-		if($tbl == 'pro')
-			$TId ='products';
-		if($tbl == 'mov' )
-			$TId ='movies';
-		$showtbl = $this->MaxId($Tid)
+		$flg = True;
+		if($tbl == 'req'){
+			$kt = 6;
+			$MTb = TableRegistry::get('requests');
+		}elseif($tbl == 'use'){
+			$kt = 6;
+			$MTb = TableRegistry::get('users');
+		}elseif($tbl == 'pro'){
+			$kt = 5;
+			$MTb = TableRegistry::get('products');
+		}elseif($tbl == 'mov' ){
+			$kt = 5;
+			$MTb = TableRegistry::get('movies');
+		}else {
+			$flg = False;
+		}
+		if($flg){
+			$query = $MTb->find();
+	      	$ret = $query->select(['max_id' => $query->func()->max('id')])->first();
+	      	$showtbl = $ret->max_id;
+			$ym = $this->ymdate();//yymm取得
+			if(round($showtbl/10**$kt,0) == $ym){
+        		$showtbl = $showtbl +1;
+    		}elseif($kt == 6){
+    			$showtbl = $ym.'000001';
+    		}else {
+    			$showtbl = $ym.'00001';
+    		}
+		}
     return $showtbl;
 	}
 
-	public function MaxId($TMId)
+	public function ymdate()
     {
-      	$requests = TableRegistry::get($TMId);
-
-      	// 新しいクエリーを始めます。
-      	$query = $requests->find();
-      	$ret = $query->select(['max_id' => $query->func()->max(1)])->first();
-      	$id = $ret->max_id;
-      	return $id;
+      	date_default_timezone_set('UTC');
+      	$dt = date("ym");//yymm生成
+      	return $dt;
     }
 }
 
