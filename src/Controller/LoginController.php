f51167961cf7e8361261e79b8cc893cb5c46357c
KawakamiTakashi <oic.t.ueda@gmail.com>
2017-11-16 14:24:56 +0900

    s

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -52,7 +52,7 @@ class LoginController extends AppController
 			$session = $this->request->session();
 			$user = $this->Auth->identify();
 			if ($user) {
-				// $this->Auth->setUser($user);
+				$this->Auth->setUser($user);
 				$session->write([
 					'id' => $user['id'],
 					'username' => $user['name'],
