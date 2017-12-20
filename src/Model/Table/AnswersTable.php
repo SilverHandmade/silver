a9fcc246b6eb476eb0cd106075a1fd4ac99013d3
gratia1107 <oic.m.hida@gmail.com>
2017-12-20 11:38:31 +0900

    answersのbake

---
+++ src/Model/Table/AnswersTable.php
@@ -0,0 +1,98 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Answers Model
+ *
+ * @property \App\Model\Table\UsersTable|\Cake\ORM\Association\BelongsTo $Users
+ *
+ * @method \App\Model\Entity\Answer get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Answer newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Answer[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Answer|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Answer patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Answer[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Answer findOrCreate($search, callable $callback = null, $options = [])
+ */
+class AnswersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('answers');
+        $this->setDisplayField('title');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->scalar('title')
+            ->requirePresence('title', 'create')
+            ->notEmpty('title');
+
+        $validator
+            ->scalar('content')
+            ->requirePresence('content', 'create')
+            ->notEmpty('content');
+
+        $validator
+            ->dateTime('Postdate')
+            ->requirePresence('Postdate', 'create')
+            ->notEmpty('Postdate');
+
+        $validator
+            ->integer('kan_flg')
+            ->requirePresence('kan_flg', 'create')
+            ->notEmpty('kan_flg');
+
+        $validator
+            ->integer('Del_flg')
+            ->requirePresence('Del_flg', 'create')
+            ->notEmpty('Del_flg');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['user_id'], 'Users'));
+
+        return $rules;
+    }
+}
