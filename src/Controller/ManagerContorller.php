556dbd7e3b44778b03c6e6853994c988ce2769eb
KawakamiTakashi <oic.t.ueda@gmail.com>
2017-12-20 10:40:04 +0900

    gin:

---
+++ src/Controller/ManagerContorller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Network\Exception\ForbiddenException;
+use Cake\Network\Exception\NotFoundException;
+use Cake\View\Exception\MissingTemplateException;
+
+class ManagerController extends AppController
+{
+	public function initialize()
+	{
+		parent::initialize();
+	}
+
+	
+
+}
